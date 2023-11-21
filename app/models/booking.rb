257e954e0d59d606440130e3fee9a238c6b2fd37
Etienne Kyburz <etienne@kyburz.me>
2023-11-21 14:22:45 +0100

    changed seedfile

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,7 @@ class Booking < ApplicationRecord
   belongs_to :user
 
   # has_one :user, through: :board # booking.board_owner gives you booking.board.user
+  # test
 
   def board_owner
     board.user
