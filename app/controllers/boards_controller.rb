5837ff44dd6d673204fc24f4ed4b86f8bf4921e2
Anhnam LeGiang anhnamlegiang@gmail.com <anhnamlegiang@gmail.com>
2023-11-21 10:53:29 +0100

    skip before action added

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,5 +1,6 @@
 class BoardsController < ApplicationController
   before_action :set_board, only: %i[ show edit update destroy ]
+  skip_before_action :authenticate_user!, only: :index
 
   # GET /boards or /boards.json
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :bookings
   resources :boards
   devise_for :users
-  root to: "pages#home"
+  root to: "boards#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
