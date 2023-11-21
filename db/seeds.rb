63d2e1b3cc6e83306b8fd206d7d96d8e486d1aa8
Etienne Kyburz <etienne@kyburz.me>
2023-11-21 14:35:04 +0100

    changed seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,13 +3,13 @@ Booking.delete_all
 Board.delete_all
 
 user1 = User.create(email: 'user1@example.com', password: 'password', first_name: 'Etienne', last_name: 'Kyburz')
-user2 = User.create(email: 'user2@example.com', password: 'password', first_name: 'Anh Nahm', last_name: 'Le Giang')
+user2 = User.create(email: 'user2@example.com', password: 'password', first_name: 'Anh Nam', last_name: 'Le Giang')
 user3 = User.create(email: 'user2@example.com', password: 'password', first_name: 'Nicolas', last_name: 'Messer')
 
 board1 = Board.create(name: 'Surfboard', availability: true, price: 50, description: 'A cool surfboard', user: user1)
 board2 = Board.create(name: 'Paddleboard', availability: true, price: 70, description: 'A not so cool Paddlboard',
                       user: user1)
-board3 = Board.create(name: 'Queen Elisabeth', availability: true, price: 9_999_999_999,
+board3 = Board.create(name: 'Queen Elisabeth', availability: true, price: 9_999_999,
                       description: 'Yep its the ship allright', user: user2)
 
 booking = Booking.create(board: board1, user: user2)
