aa9a0eae89079f79690c0429aa9a0ed35e9b8749
Etienne Kyburz <etienne@kyburz.me>
2023-11-22 15:05:37 +0100

    merging finish

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,12 @@
 class PagesController < ApplicationController
-  before_action :authenticate_user!, only: [:profile]
-
   def profile
     @user = current_user
     @boards = @user.boards
     @board = Board.new
+  end
+
+  def myboards
+    @user = current_user
+    @boards = current_user.boards
   end
 end
