fc0c47f66ec16f07267872af6cb991b223510b97
Etienne Kyburz <etienne@kyburz.me>
2023-11-22 15:10:14 +0100

    routes changes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
 
-  get 'myboards' => 'pages#myboards', as: :myboards
+  get '/profile/myboards' => 'pages#myboards'
   # Defines the root path route ("/")
   # root "posts#index"
 end
